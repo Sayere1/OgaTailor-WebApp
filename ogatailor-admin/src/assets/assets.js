8df4a9d03af0da6a02a92f02eef0c819c65c83d6
Sayere1 <122838434+Sayere1@users.noreply.github.com>
2024-08-27 09:35:31 +0100

    updated to include all asset

--- ogatailor-admin/src/assets/assets.js
+++ ogatailor-admin/src/assets/assets.js
@@ -5,6 +5,8 @@ import profile_image from './profile_image.png'
 import upload_area from './upload_area.png'
 import parcel_icon from './parcel_icon.png'
 
+// export
+
 export const url = 'http://localhost:4000'
 export const currency = '$'
 
